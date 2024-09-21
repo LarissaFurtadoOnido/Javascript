27a45e28ff30cc170994bc624b284023c3fd9282
LarissaFurtadoOnido <olarissafurtado@gmail.com>
2024-09-21 12:29:15 -0300

    começo

---
+++ ex1/index.js
@@ -0,0 +1,27 @@
+/*Crie uma calculadora que permita ao usuário inserir dois números e selecionar uma
+operação (adição, subtração, multiplicação ou divisão) usando botões. O resultado
+deve ser exibido abaixo do formulário.
+Exemplo de Entrada:
+Número 1: 10
+Número 2: 5
+
+Operação: Multiplicação
+
+Exemplo de Saída:
+Resultado: 50 */
+
+/*function calculadora(num1, num2, operacao){
+    if (operacao == '+'){
+        return num1 + num2;
+    }else if(operacao == '-'){
+        return num1 - num2;
+    }else if(operacao == '*'){
+        return num1 * num2;
+    }else{
+        return num1 / num2;
+    }
+}
+console.log(calculadora(1, 2, "+"))
+*/
+
+
